5a714df07a98adfec6abf818832bf77a22c1a065
Ana Berrocal <anaberrocal@ymail.com>
2020-02-19 18:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]} .`;
+    return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
    }
  };
 
